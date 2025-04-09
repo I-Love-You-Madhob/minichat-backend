860a86256f340b2193156736c14eeddb742a0286
I-Love-You-Madhob <aniruddho.official@gmail.com>
2025-04-10 00:58:26 +0600

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const server = createServer(app)
 
 const io = new Server(server, {
     cors:{
-        origin:process.env.FRONTEND_URL,
+        origin:"*",
         methods:["GET","POST"],
         credentials:true
     }
